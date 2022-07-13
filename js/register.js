62d50ff5743c6038648d31e4f52c868ce477a599
Fernelis Martinez <afmrfernelis@gmail.com>
2022-07-12 23:24:19 -0400

    seguimos

---
+++ js/register.js
@@ -0,0 +1,40 @@
+let registro={name: '', lastname: '', dateOfBirth: '',placeOfBirth: '',nationality: '', identityCard: '', adress: '',
+    phone: '',email: '', facebook: '', instagram: '',academicLeve: '',numberOfChildren: '', dateOfConversion: '', sex: '',child: '',
+    position: '',maritalStatusl: '', ministry: '', status: ''};
+
+
+let name = document.getElementById("name");
+let lastname = document.getElementById("lastname");
+let dateOfBirth = document.getElementById("dateOfBirth");
+let placeOfBirth = document.getElementById("placeOfBirth");
+let nationality= document.getElementById("nationality");
+let identityCard = document.getElementById("identity");
+let adress = document.getElementById("adress");
+let phone = document.getElementById("phone");
+let email = document.getElementById("email");
+let facebook = document.getElementById("facebook");
+let instagram = document.getElementById("instagram");
+let academicLevel = document.getElementById("academicLevel");
+let numberOfChildren = document.getElementById("numberOfChildren");
+let dateOfConversion = document.getElementById("dateOfConversion");
+let sex = document.getElementById("sex");
+let child= document.getElementById("child");
+let position = document.getElementById("position");
+let maritalStatus = document.getElementById("maritalStatus");
+let ministry= document.getElementById("ministry");
+let status = document.getElementById("status");
+
+
+register =()=>{
+    name.value
+
+
+}
+
+
+
+
+
+
+
+
